645408f26a00bfab0adbb9cceba696f5981d3c90
JULESlois <hxdccw@gmail.com>
2026-08-25 18:06:02 +0800

    remove duplicate about scroll handling

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,24 +1,8 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import { Hero } from "../components/Hero";
 import { Timeline } from "../components/Timeline";
 import { BackgroundMusic } from "../components/BackgroundMusic";
 
 export function AboutPage() {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.hash) {
-      const id = location.hash.substring(1);
-      const element = document.getElementById(id);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth", block: "start" });
-      }
-    } else {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }
-  }, [location.hash]);
-
   return (
     <>
       <BackgroundMusic enableExternalMusicControl />
